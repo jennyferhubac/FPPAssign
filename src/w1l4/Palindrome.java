ec109fa176cc675111a24a96e38c64d51f3f9155
984864 <984864@CS_LAB113-107.cs.mum.edu>
2015-11-30 14:04:53 -0600

    commit

---
+++ src/w1l4/Palindrome.java
@@ -0,0 +1,24 @@
+package w1l4;
+
+public class Palindrome {
+
+	public static void main(String args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+	
+	/*
+	public static boolean isPalindrome(String str)
+	{
+		if(str == null || str == "")
+			return true;
+		else
+		{
+			
+		}
+		
+
+		
+	}
+		*/
+}

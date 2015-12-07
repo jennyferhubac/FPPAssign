a4c53a058888981ebcf3db1b8f995e144ad387eb
984864 <984864@CS_LAB113-107.cs.mum.edu>
2015-12-07 17:22:52 -0600

    Lesson 8 assignment 2 modification

--- src/lesson8/question2/Marketing.java
+++ src/lesson8/question2/Marketing.java
@@ -11,5 +11,19 @@ public class Marketing {
 		this.productName = productName;
 		this.salesAmount = salesAmount;
 	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("<");
+		sb.append("Employee Name: ");
+		sb.append(employeeName);
+		sb.append(" Product Name: ");
+		sb.append(productName);
+		sb.append(" Sales Amount: ");
+		sb.append(salesAmount);
+		sb.append(">");
+		return sb.toString();
+		
+	}
 
 }
--- src/lesson8/question2/MarketingList.java
+++ src/lesson8/question2/MarketingList.java
@@ -2,8 +2,31 @@ package lesson8.question2;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MarketingList {
+	
+	static enum SortMethod {BYEMPLOYEE, BYSALES};
+	private boolean ignoreCase = true;
+	public void setIgnoreCase(boolean b) {
+		ignoreCase = b;
+	}
+	public void sort(List<Marketing> market, SortMethod method) {
+		Collections.sort(market, (e1,e2) ->
+		{
+			if(method == SortMethod.BYEMPLOYEE) {
+				if(ignoreCase) return e1.employeeName.compareToIgnoreCase(e2.employeeName);
+				else return e1.employeeName.compareTo(e2.employeeName);
+			} else {
+				if(e1.salesAmount == e2.salesAmount) return 0;
+				else if(e1.salesAmount < e2.salesAmount) return -1;
+				else return 1;
+			}		
+		});
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,7 +37,12 @@ public class MarketingList {
 		
 		Marketing m = mList.get(2);
 		
+		System.out.println("Employee in index 2:");
 		System.out.println("Employee: " + m.employeeName + " Product Name: " + m.productName + " Sales Amount: " + m.salesAmount);
+		
+		System.out.println("List of employees: ");
+		System.out.println(mList);
+		System.out.println("Size of the list: " + mList.size());
 		
 		NumberFormat currency = NumberFormat.getCurrencyInstance();
 		double totSale = 0.0;
@@ -26,6 +54,21 @@ public class MarketingList {
 		
 		System.out.println("Total sales amount of Brand C is: " + currency.format(totSale));
 		
+		mList.remove(1);
+		
+		System.out.println("List of employees after removing index 1.");
+		
+		mList.set(0, new Marketing("Regan", "Brand Y", 4550.00));
+		
+		MarketingList marList = new MarketingList();
+		marList.sort(mList, SortMethod.BYEMPLOYEE);
+		
+		System.out.println(mList);
+		
+		marList.sort(mList, SortMethod.BYSALES);
+		System.out.println(mList);
+		
+		System.out.println("Size of the list: " + mList.size());
 	}
 	
 	public static void fillMarketingList(ArrayList<Marketing> mList)

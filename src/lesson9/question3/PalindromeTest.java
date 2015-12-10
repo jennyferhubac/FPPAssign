9a7cac8bec121ef866e9d658ca71ffb40cb4d8da
Jennyfer <jennyfer.hubac@gmail.com>
2015-12-09 21:35:30 -0600

    added predefined stack implementation

--- src/lesson9/question3/PalindromeTest.java
+++ src/lesson9/question3/PalindromeTest.java
@@ -1,13 +1,21 @@
 package lesson9.question3;
 
+import java.util.Stack;
+
 public class PalindromeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("=============Using Array Stack=========");
 		System.out.println("Is aabbaa palindrome? "+ isPalindrome("aabbaa"));
 		System.out.println("Is avavav palindrome? "+ isPalindrome("avavav"));
 		System.out.println("Is 110011 palindrome? "+ isPalindrome("110011"));
+		System.out.println("=============Using predefined Stack=============");
+		System.out.println("Is aabbaa palindrome? "+ isPalindrome("aabbaa"));
+		System.out.println("Is avavav palindrome? "+ isPalindrome("avavav"));
+		System.out.println("Is 110011 palindrome? "+ isPalindrome("110011"));
+		
 
 	}
 	
@@ -18,6 +26,19 @@ public class PalindromeTest {
 		if(str.length() != 0)
 		{
 			if(str.equalsIgnoreCase(reverseString(str)))
+				isPalindrome = true;
+		}
+		
+		return isPalindrome;
+	}
+	
+	public static boolean isPalindromeUsingPredefinedStack(String str)
+	{
+		boolean isPalindrome = false;
+		
+		if(str.length() != 0)
+		{
+			if(str.equalsIgnoreCase(reverseStringUsingPredefinedStack(str)))
 				isPalindrome = true;
 		}
 		
@@ -41,6 +62,25 @@ public class PalindromeTest {
 		}
 		
 		return reverseStr;
+	}
+	
+	public static String reverseStringUsingPredefinedStack(String str)
+	{
+		Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+
+        String reverseInput = "";
+
+        while (!stack.isEmpty()) {
+            reverseInput += stack.pop();
+        }
+        
+        return  reverseInput;
+        	
+
 	}
 
 }
